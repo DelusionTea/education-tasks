b8f0b217858da43695f48b48d52215ba80762c14
DelusionTea <95547364+DelusionTea@users.noreply.github.com>
2021-12-20 01:30:30 +0300

    Create Context.go
    education task Context

---
+++ Context.go
@@ -0,0 +1,66 @@
+package main
+
+//----------
+/*Вам нужно дописать строчки кода:
+Получение сигнала отмены контекста.
+Отмена контекста через 2500 миллисекунд.
+package main*/
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+type DB struct {
+}
+
+type User struct {
+	Name string
+}
+
+func (d *DB) SelectUser(ctx context.Context, email string) (User, error) {
+	timer := time.NewTimer(1 * time.Second)
+	select {
+	case <-timer.C:
+		return User{Name: "Kindertei"}, nil
+	case <-ctx.Done():
+		return User{}, fmt.Errorf("context canceled")
+	}
+}
+
+type Handler struct {
+	db *DB
+}
+
+type Request struct {
+	Email string
+}
+
+type Response struct {
+	User User
+}
+
+func (h *Handler) HandleAPI(ctx context.Context, req Request) (Response, error) {
+	u, err := h.db.SelectUser(ctx, req.Email)
+	if err != nil {
+		return Response{}, err
+	}
+
+	return Response{User: u}, nil
+}
+
+func main() {
+	db := DB{}
+	handler := Handler{db: &db}
+	ctx, cancel := context.WithCancel(context.Background())
+
+	time.AfterFunc(2500*time.Millisecond, cancel)
+
+	// когда запустите код и он отработает успешно,
+	// попробуйте заменить длительность на 2000 миллисекунд
+
+	req := Request{Email: "test@yandex.ru"}
+	resp, err := handler.HandleAPI(ctx, req)
+	fmt.Println(resp, err)
+}
